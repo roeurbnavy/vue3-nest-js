c1ee916df6231e29ff65c88f2596bdd694ebb4e1
Navy <roeurbnavy@gmail.com>
2024-05-21 11:22:07 +0700

    check secretOrKey in JwtStrategy

--- backend/src/core/auth/jwt.strategy.ts
+++ backend/src/core/auth/jwt.strategy.ts
@@ -1,36 +1,36 @@
-import { ConfigService } from '@nestjs/config';
-import { UsersService } from './../users/users.service';
-import { Strategy, ExtractJwt } from 'passport-jwt';
-import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config'
+import { UsersService } from './../users/users.service'
+import { Strategy, ExtractJwt } from 'passport-jwt'
+import { PassportStrategy } from '@nestjs/passport'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     private readonly userService: UsersService,
-    readonly config: ConfigService,
+    readonly config: ConfigService
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: config.get('JWT_KEY'),
-    });
+      secretOrKey: config.get('JWT_KEY') ?? 'secretKey',
+    })
   }
 
   async validate({ iat, exp, id }, done) {
-    const timeDiff = exp - iat;
+    const timeDiff = exp - iat
     if (timeDiff <= 0) {
-      console.log('validate timeDiff', timeDiff);
-      throw new UnauthorizedException();
+      console.log('validate timeDiff', timeDiff)
+      throw new UnauthorizedException()
     }
-    const user = await this.userService.findUserById(id);
+    const user = await this.userService.findUserById(id)
     if (!user) {
-      console.log('validate UnauthorizedException');
-      throw new UnauthorizedException();
+      console.log('validate UnauthorizedException')
+      throw new UnauthorizedException()
     }
-    // delete user.password;
-    const { password, ...result } = user;
+    // delete user.password
+    const { password, ...result } = user
 
-    done(null, result);
+    done(null, result)
   }
 }
