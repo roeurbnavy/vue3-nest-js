e9856ff7010d0f7dffd109ac7f4dd002c120baab
Navy <roeurbnavy@gmail.com>
2024-05-21 10:01:01 +0700

    remove mongo connection for deploy

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -21,11 +21,11 @@ import { CustomersModule } from './modules/customers/customers.module'
       // envFilePath,
       // isGlobal: true,
     }),
-    MongooseModule.forRootAsync(mongoAsyncConfig),
+    // MongooseModule.forRootAsync(mongoAsyncConfig),
     // connection to pos
-    // MongooseModule.forRoot('mongodb://localhost:27017/leang_srun', {
-    //   connectionName: 'pos',
-    // }),
+    MongooseModule.forRoot('mongodb://localhost:27017/leang_srun', {
+      connectionName: 'pos',
+    }),
     // CoreModule,
     // TodoModule,
     // CustomersModule,
